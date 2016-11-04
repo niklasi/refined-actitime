623714964668563b86cc309566f67fc612bc4152
Niklas Ingholt <niklas@ingholt.com>
2016-11-04 15:18:24 +0100

    Remove logging.

--- extension/time-tracking.js
+++ extension/time-tracking.js
@@ -3,7 +3,6 @@ chrome.storage.sync.get({ taskId: 197, hoursPerDay: 8 }, function (items) {
   $(`#taskRow${items.taskId} .calendarWorkingDayNormal :text.inputTT, #taskRow${items.taskId} .calendarCurrentDayNormal :text.inputTT`).each((i, el) => {
     const $el = $(el)
     if ($el.val() === '') {
-      console.log('items', items)
       $el.val(items.hoursPerDay)
     } else {
       $el.css('background-color', 'rgba(49, 128, 0, 0.32)')
