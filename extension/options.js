0ea025d7722e98d6fcecadf27af4c932299e780b
Niklas Ingholt <niklas@ingholt.com>
2016-11-04 14:55:24 +0100

    Configure default task and time per day.

---
+++ extension/options.js
@@ -0,0 +1,26 @@
+/* global chrome */
+// Saves options to chrome.storage.sync.
+function saveOptions () {
+  var taskId = document.getElementById('default-task').value
+  var timePerDay = document.getElementById('default-time').value
+  chrome.storage.sync.set({ taskId, timePerDay }, function () {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status')
+    status.textContent = 'Options saved.'
+    setTimeout(function () {
+      status.textContent = ''
+    }, 750)
+  })
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restoreOptions () {
+  // Use default value color = 'red' and likesColor = true.
+  chrome.storage.sync.get({ taskId: 197, timePerDay: 8 }, function (items) {
+    document.getElementById('default-task').value = items.taskId
+    document.getElementById('default-time').value = items.timePerDay
+  })
+}
+document.addEventListener('DOMContentLoaded', restoreOptions)
+document.getElementById('save').addEventListener('click', saveOptions)
