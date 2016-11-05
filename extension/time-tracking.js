870f308a6bcb079f013f553705a30ae9055f1ee6
Niklas Ingholt <niklas@ingholt.com>
2016-11-05 12:39:25 +0100

    Improve detection of none working days.

--- extension/time-tracking.js
+++ extension/time-tracking.js
@@ -1,8 +1,9 @@
 /* global $, chrome */
 chrome.storage.sync.get({ taskId: 197, hoursPerDay: 8 }, function (items) {
-  $(`#taskRow${items.taskId} .calendarWorkingDayNormal :text.inputTT, #taskRow${items.taskId} .calendarCurrentDayNormal :text.inputTT`).each((i, el) => {
+  $(`#taskRow${items.taskId} :text.inputTT`).each((i, el) => {
+    const workingDay = $(`#leaveButton_${i}_table.disabled`).length === 0
     const $el = $(el)
-    if ($el.val() === '') {
+    if ($el.val() === '' && workingDay) {
       $el.val(items.hoursPerDay)
       $el.css('background-color', 'rgba(255, 235, 141, 0.58)')
     }
