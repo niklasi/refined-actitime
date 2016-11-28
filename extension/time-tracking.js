9f78664ec66d5b12a80e41f5afb47aa17cb93bd5
Niklas Ingholt <niklas@ingholt.com>
2016-11-28 21:27:11 +0100

    Remove jquery dependency.

--- extension/time-tracking.js
+++ extension/time-tracking.js
@@ -1,35 +1,52 @@
-/* global $, chrome */
+/* global chrome */
 
-$('.taskRow').each((i, el) => {
-  const taskId = $(el).attr('id').replace('taskRow', '')
+function eachTaskRow (cb) {
+  const taskRows = [].slice.call(document.getElementsByClassName('taskRow'))
+  taskRows.forEach(cb)
+}
 
-  const $task = $('span.task', el)
-  const taskName = $task.text()
+eachTaskRow((el, i) => {
+  const taskId = el.getAttribute('id').replace('taskRow', '')
 
-  $task.on('mouseover', () => {
-    $task.text(`${taskName} (taskId: ${taskId})`)
+  const task = el.getElementsByClassName('task')[0]
+
+  const taskName = task.innerText
+
+  task.addEventListener('mouseover', () => {
+    task.innerText = `${taskName} (taskId: ${taskId})`
   })
 
-  $task.on('mouseout', () => {
-    $task.text(taskName)
+  task.addEventListener('mouseout', () => {
+    task.innerText = taskName
   })
 })
 
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    if (request.message === 'ready') {
-      chrome.storage.sync.get({ taskId: 197, hoursPerDay: '8:00' }, function (items) {
-        $(`#taskRow${items.taskId} :text.inputTT`).each((i, el) => {
-          const workingDay = $(`#leaveButton_${i}_table.disabled`).length === 0
-          const $el = $(el)
-          $el.focus()
-          if ($el.val() === '' && workingDay) {
-            $el.val(items.hoursPerDay)
-            $el.css('background-color', 'rgba(255, 235, 141, 0.58)')
-          }
-          $el.blur()
-        })
-      })
-    }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === 'ready') {
+    fillDefaultHours()
   }
+}
 )
+
+function hasClass (el, className) {
+  for (let item of el.classList.values()) {
+    if (item === className) return true
+  }
+
+  return false
+}
+
+function fillDefaultHours () {
+  chrome.storage.sync.get({ taskId: 197, hoursPerDay: '8:00' }, function (items) {
+    [0, 1, 2, 3, 4, 5, 6]
+      .filter(i => !hasClass(document.getElementById(`leaveButton_${i}_table`), 'disabled'))
+      .map(i => document.getElementById(`spent_${items.taskId}_${i}`))
+      .filter(el => el.value === '')
+      .forEach(el => {
+        el.focus()
+        el.value = items.hoursPerDay
+        el.style.backgroundColor = 'rgba(255, 235, 141, 0.58)'
+        el.blur()
+      })
+  })
+}
