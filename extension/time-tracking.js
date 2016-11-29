417e10b1ca8ccb86df910400c1f5aaa26e3f4eac
Niklas Ingholt <niklas@ingholt.com>
2016-11-29 23:12:05 +0100

    Auto fill for passed and current dates.

--- extension/time-tracking.js
+++ extension/time-tracking.js
@@ -25,8 +25,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === 'ready') {
     fillDefaultHours()
   }
-}
-)
+})
 
 function hasClass (el, className) {
   for (let item of el.classList.values()) {
@@ -36,13 +35,41 @@ function hasClass (el, className) {
   return false
 }
 
+const leaveButtons = [
+  document.getElementById(`leaveButton_0_table`),
+  document.getElementById(`leaveButton_1_table`),
+  document.getElementById(`leaveButton_2_table`),
+  document.getElementById(`leaveButton_3_table`),
+  document.getElementById(`leaveButton_4_table`),
+  document.getElementById(`leaveButton_5_table`),
+  document.getElementById(`leaveButton_6_table`)
+]
+
+function workingDay (i) {
+  return !hasClass(leaveButtons[i], 'disabled')
+}
+
+function noLeaveTime (i) {
+  return leaveButtons[i].title === 'Click to enter leave time'
+}
+
+function getStartDate () {
+  const value = document.getElementsByTagName('input')['dateStr'].value
+  return new Date(Date.parse(`${value.substr(0, 4)}-${value.substr(4, 2)}-${value.substr(6, 2)}`))
+}
+
+function notInFuture (i) {
+  let date = getStartDate()
+  date.setDate(date.getDate() + i)
+  return new Date().toISOString().substring(0, 10) >= date.toISOString().substring(0, 10)
+}
+
 function fillDefaultHours () {
   chrome.storage.sync.get({ taskId: 197, hoursPerDay: '8:00' }, function (items) {
     [0, 1, 2, 3, 4, 5, 6]
-      .filter(i => {
-        const leaveButton = document.getElementById(`leaveButton_${i}_table`)
-        return !hasClass(leaveButton, 'disabled') && leaveButton.title === 'Click to enter leave time'
-      })
+      .filter(i => notInFuture(i))
+      .filter(i => workingDay(i))
+      .filter(i => noLeaveTime(i))
       .map(i => document.getElementById(`spent_${items.taskId}_${i}`))
       .filter(el => el.value === '')
       .forEach(el => {
